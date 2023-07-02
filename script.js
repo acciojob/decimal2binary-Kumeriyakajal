1883370d5d84559381e6ef3ebabd098c813b6f8d
ketki kumeriya <111123802+Kumeriyakajal@users.noreply.github.com>
2023-07-02 16:57:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+  let num = 7;
+  while (num > 0) {
+    let remainder = decimal % 2;
+    binary = remainder + binary;
+    decimal = Math.floor(decimal / 2);
+  }
+  return binary;
 }
 
 window.decimalToBinary = decimalToBinary;
